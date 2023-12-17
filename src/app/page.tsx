7cd099da8dc7e0ea359a882cdbc00e6fa6fe9f90
Yegor Kravchenko <kraver.kravchenko@gmail.com>
2023-12-17 20:44:20 +0100

    Add create todo form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import HelloWorld from '@/components/helloworld';
+import { createTodoAction } from './actions';
+import { db } from '@/db';
 
-export default function Home() {
+export default async function Home() {
+  const todos = await db.query.todos.findMany();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <HelloWorld />
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <form
+        className="border-b border-solid border-white py-6 px-3"
+        action={createTodoAction}
+      >
+        <input
+          className="border border-solid border-white text-black px-2 py-1 rounded bg-gradient-to-br from-slate-50 to-slate-200"
+          name="text"
+          type="text"
+          placeholder="What do you have to do?"
+          autoComplete="off"
+        />
+        <button className="border border-solid border-white rounded ml-4 px-2 py-1 hover:bg-gradient-to-br from-slate-50 to-slate-300 hover:text-black">
+          Create todo
+        </button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </main>
   );
 }
